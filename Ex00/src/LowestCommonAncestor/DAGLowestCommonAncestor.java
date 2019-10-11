02ae5f247733aa53fbb5b04854aaf25da5f57657
David <davidhooban@hotmail.com>
2019-10-12 00:59:28 +0100

    added implementation for LCA algorithm that passes all basic tests

--- Ex00/src/LowestCommonAncestor/DAGLowestCommonAncestor.java
+++ Ex00/src/LowestCommonAncestor/DAGLowestCommonAncestor.java
@@ -62,10 +62,75 @@ public class DAGLowestCommonAncestor {
 		return reverseAdj[v]; 
 	}
 	
-	public Integer lowestCommonAncestor(int x, int y) {
-		//TODO
-		return null;
+	public Integer lowestCommonAncestor(int x, int y)
+	{
+		ArrayList<Integer> lcas = new ArrayList<Integer>();
+		int currentMaxDist = Integer.MAX_VALUE;
+		
+		if(x==y || x>=this.V || y>=this.V || x<0 || y<0) { return null; } //If invalid input return null
+		
+		DirectedDFS dfsObject = new DirectedDFS(this, x);
+		dfsObject.reverseDfs(this, x);
+		int xDist, yDist;
+		
+		for(int v = 0; v < this.V; v++)
+		{
+		
+			if(dfsObject.revVisited(v) && hasPath(v, y))
+			{
+				xDist = getDistance(v, x);
+				yDist = getDistance(v, y);
+				
+				if(Integer.max(xDist, yDist) < currentMaxDist)
+				{		
+					lcas = new ArrayList<Integer>();
+					lcas.add(v);
+					currentMaxDist = Integer.max(xDist, yDist);
+				}
+				else if(Integer.max(xDist, yDist) == currentMaxDist)
+				{
+					lcas.add(v);
+					currentMaxDist = Integer.max(xDist, yDist);
+				}
+			}
+		}
+		return lcas.get(0);
 	}
+	
+	private int getDistance(int x, int target)
+	{
+			if( x == target) { 
+				return 0; 
+			}
+			else {
+		        Queue<Integer> q = new LinkedList<Integer>();
+		        int[] distTo = new int[this.V];
+		        boolean[] marked = new boolean[this.V];
+		        
+		        for (int v = 0; v < this.V(); v++)
+		        {   distTo[v] = Integer.MAX_VALUE;}
+		        
+		        distTo[x] = 0;
+		        marked[x] = true;
+		        q.add(x);
+		
+		        while (!q.isEmpty()) {
+		            int v = q.remove();
+		            for (int w : this.adj(v)) {
+		                if (!marked[w]) {
+		                	
+		                	distTo[w] = distTo[v] + 1;
+		                    marked[w] = true;
+		                    
+		                    q.add(w);
+		                }
+		            }
+		        }
+		        
+		        return distTo[target];
+			}
+	}
+	
 	
 	// Class to create a depth first search object on a directed graph.
 	private class DirectedDFS
